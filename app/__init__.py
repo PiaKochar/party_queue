e167a3ce48884614a21da6cbe64fad5a01c42021
charlesliwang <charlesliwang76@gmail.com>
2016-04-25 18:02:43 -0400

    fixed pias dumb mistake

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,6 @@ class Queue(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120))
     password = db.Column(db.String(80))
-    songs = 
 
     def __init__(self, name, password):
         self.name = name
