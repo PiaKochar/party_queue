640713fab47ea550513dfce74ee629c0b5d8d0a1
charlesliwang <charlesliwang76@gmail.com>
2016-04-25 17:56:23 -0400

    adding interface stuff

--- app/views.py
+++ app/views.py
@@ -1,11 +1,22 @@
 from flask import render_template
+from flask import request
 from app import app
 import spotipy
 import spotipy.util as util
+# import spotify
+
+# config = spotify.Config()
+# config.user_agent = 'My awesome Spotify client'
+# config.tracefile = b'/tmp/libspotify-trace.log'
+# session = spotify.Session(config)
 
 @app.route('/')
 @app.route('/index')
 def index():
+    # track = session.get_track('spotify:track:1ToxbPCzdozcOPAe0Pyuyk')
+    # player = Player(session)
+    # player.load(track)
+    # player.play
     birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
     spotify = spotipy.Spotify()
 
@@ -14,9 +25,35 @@ def index():
     while results['next']:
         results = spotify.next(results)
         albums.extend(results['items'])
-    
     user = {'nickname': 'Miguel'}
     return render_template("index.html",
                            title='Home',
                            user=user,
                            albums=albums)
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+  text = request.form['text']
+  processed_text = text.lower()
+  return results(processed_text)
+
+@app.route('/results')
+def results(search):
+  spotify = spotipy.Spotify()
+  results = spotify.search(q='track:' + search, type='track')
+  items = results['tracks']['items']
+  tracks = []
+  for i in range(10):
+    track = items[i]
+    # for key in track:
+    #   print "key: %s , value: %s" % (key, track[key])
+    # tracks.append(track['name'] + " " + track['artists'][0]['name'] + \
+    #  " " + track['uri'])
+    tracks.append(track)
+
+
+  return render_template("results.html",
+                           title='Home',
+                           tracks=tracks)
+
+
