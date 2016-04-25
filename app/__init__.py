c77c8bee9001ccbe69dd55bde07df6cd3bf22ce7
Pia Kochar <piakochar11@gmail.com>
2016-04-25 17:57:15 -0400

    initialized database

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 from app import views
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
+class Queue(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+    password = db.Column(db.String(80))
+    songs = 
+
+    def __init__(self, name, password):
+        self.name = name
+        self.password = password
+
+    def __repr__(self):
+        return '<Queue %r>' % self.name
