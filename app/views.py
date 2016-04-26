54ce0a6ee860fc5c919be965ee50c7ef935e0e87
Samy Lanka <lankas@sas.upenn.edu>
2016-04-26 19:17:57 -0400

    initial login and registration

--- app/views.py
+++ app/views.py
@@ -1,5 +1,4 @@
-from flask import render_template, request, flash
-# from flask import request,
+from flask import render_template, request, flash, session, redirect, url_for
 from app import app
 import spotipy
 import spotipy.util as util
@@ -10,28 +9,59 @@ import spotipy.util as util
 # config.tracefile = b'/tmp/libspotify-trace.log'
 # session = spotify.Session(config)
 
+def is_logged_in():
+    if 'username' in session:
+        return True
+    return False
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if is_logged_in():
+        redirect(url_for('index'))
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        # user = User(request.form['username'])
+        # db_session.add(user)
+        # db_session.commit()
+        return redirect(url_for('index'))
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if is_logged_in():
+        return redirect(url_for('index'))
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('login'))
+
+# set the secret key.  keep this really secret:
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+
 @app.route('/')
 @app.route('/index')
 def index():
-  birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
-  spotify = spotipy.Spotify()
+    if not is_logged_in():
+        return redirect(url_for('login'))
+    birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
+    spotify = spotipy.Spotify()
 
-  results = spotify.artist_albums(birdy_uri, album_type='album')
-  albums = results['items']
-  while results['next']:
-      results = spotify.next(results)
-      albums.extend(results['items'])
-  user = {'nickname': 'Miguel'}
-  return render_template("index.html",
-                          title='Home',
-                          user=user,
-                          albums=albums)
-
-# @app.route('/', methods=['POST'])
-# def my_form_post():
-#   text = request.form['text']
-#   processed_text = text.lower()
-#   return results(processed_text)
+    results = spotify.artist_albums(birdy_uri, album_type='album')
+    albums = results['items']
+    while results['next']:
+        results = spotify.next(results)
+        albums.extend(results['items'])
+    user = {'nickname': 'Miguel'}
+    return render_template("index.html",
+                           title='Home',
+                           user=user,
+                           albums=albums)
 
 @app.route('/', methods=['POST'])
 def my_form_post():
@@ -50,24 +80,26 @@ def my_form_post():
 
 @app.route('/results')
 def results(search):
-  # if request.form['submit'] == 'Add to Party':
-  #     song = Song(track['name'], queue name) # how to get this data?
-  #     db.session.add(song)
-  #     db.session.commit()
-  spotify = spotipy.Spotify()
-  results = spotify.search(q='track:' + search, type='track')
-  items = results['tracks']['items']
-  tracks = []
-  for i in range(10):
-    track = items[i]
+    if not is_logged_in():
+        return redirect(url_for('login'))
+    # if request.form['submit'] == 'Add to Party':
+    #     song = Song(track['name'], queue name) # how to get this data?
+    #     db.session.add(song)
+    #     db.session.commit()
+    spotify = spotipy.Spotify()
+    results = spotify.search(q='track:' + search, type='track')
+    items = results['tracks']['items']
+    tracks = []
+    for i in range(10):
+        track = items[i]
       # for key in track:
-      #   print "key: %s , value: %s" % (key, track[key])
+      #     print "key: %s , value: %s" % (key, track[key])
       # tracks.append(track['name'] + " " + track['artists'][0]['name'] + \
       #  " " + track['uri'])
-    tracks.append(track)
+        tracks.append(track)
 
 
-  return render_template("results.html",
+    return render_template("results.html",
                           title='Home',
                           tracks=tracks)
 
